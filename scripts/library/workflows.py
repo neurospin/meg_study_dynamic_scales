2c55c93f91b3f914aa3a2bd96d780f4d18e10d21
dengemann <denis.engemann@gmail.com>
2016-04-10 10:45:18 +0100

    updates

--- scripts/library/workflows.py
+++ scripts/library/workflows.py
@@ -30,7 +30,7 @@ def _psd_average_sensor_space(
     picks = mne.pick_types(raw.info, meg=True, ref_meg=False)
     epochs = mne.Epochs(
         raw, picks=picks, events=events, event_id=3000, tmin=0, tmax=duration,
-        detrend=1,
+        detrend=1, baseline=None,
         reject=dict(mag=5e-12), preload=True, decim=decim, proj=True)
 
     X_psds = 0.0
@@ -43,17 +43,53 @@ def _psd_average_sensor_space(
     mne_psds = mne.EvokedArray(
         data=X_psds, info=deepcopy(epochs.info), tmin=0, nave=1)
     hcp.preprocessing.transform_sensors_to_mne(mne_psds)
-    return mne_psds, freqs, epochs.info
+    return mne_psds, freqs
+
+
+def _psd_epochs_sensor_space(
+        subject, run_index, recordings_path, fmin, fmax,
+        hcp_path, n_ssp, decim,  mt_bandwidth, duration, n_jobs):
+
+    raw = hcp_preprocess_ssp_ica(
+        subject=subject, run_index=run_index, recordings_path=recordings_path,
+        hcp_path=hcp_path, fmin=fmin, fmax=fmax, n_jobs=n_jobs, n_ssp=n_ssp)
+
+    events = mne.make_fixed_length_events(raw, 3000, duration=duration)
+    picks = mne.pick_types(raw.info, meg=True, ref_meg=False)
+    epochs = mne.Epochs(
+        raw, picks=picks, events=events, event_id=3000, tmin=0, tmax=duration,
+        detrend=1, baseline=None,
+        reject=dict(mag=5e-12), preload=True, decim=decim, proj=True)
+    epochs.apply_proj()
+
+    X_psds, freqs = psd_multitaper(
+        epochs, fmin=fmin, fmax=fmax, bandwidth=mt_bandwidth, n_jobs=1)
+
+    info = deepcopy(epochs.info)
+    info['projs'] = list()
+    events = epochs.events.copy()
+    del epochs
+    mne_psds = mne.EpochsArray(data=X_psds, info=info, events=events, tmin=0)
+    hcp.preprocessing.transform_sensors_to_mne(mne_psds)
+    return mne_psds, freqs
 
 
 def compute_power_sepctra(
         subject, recordings_path, fmin=None, fmax=200,
         hcp_path=op.curdir, n_ssp=12, decim=1, mt_bandwidth=4, duration=30,
         report=None, dpi=300, n_jobs=1, results_dir=None, run_inds=(0, 1, 2),
-        run_id=None):
+        run_id=None, out='average'):
+    if out == 'average':
+        fun = _psd_average_sensor_space
+        file_tag = 'ave'
+    elif out == 'epochs':
+        fun = _psd_epochs_sensor_space
+        file_tag = 'epo'
+    else:
+        raise ValueError('"out" must be "epochs" or "average"')
 
     for run_index in run_inds:
-        mne_psds, freqs, info = _psd_average_sensor_space(
+        mne_psds, freqs = fun(
             subject=subject, run_index=run_index,
             recordings_path=recordings_path, fmin=fmin, fmax=fmax,
             hcp_path=hcp_path, n_ssp=n_ssp, decim=decim,
@@ -62,10 +98,12 @@ def compute_power_sepctra(
 
         written_files = list()
         written_files.append(
-            op.join(recordings_path, subject, 'psds-%i-ave-%i-%i-ave.fif' % (
-                run_index, int(0 if fmin is None else fmin), int(fmax))))
+            op.join(recordings_path, subject, 'psds-r%i-%i-%i-%s.fif' % (
+                run_index, int(0 if fmin is None else fmin), int(fmax),
+                file_tag)))
         fig_log = plot_loglog(
-            mne_psds.data, freqs[(freqs >= fmin) & (freqs <= fmax)],
+            mne_psds.average().data if out == 'epochs' else mne_psds.data,
+            freqs[(freqs >= fmin) & (freqs <= fmax)],
             xticks=(0.1, 1, 10, 100))
         fig_log.set_dpi(dpi)
         if report is not None:
